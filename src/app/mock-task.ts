7a942828e35b662f857ff13b46e37709ee3c1bf1
David Lerma <davidlerma87@hotmail.com>
2021-07-19 16:08:25 -0500

    first commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'July 9th at 12:21',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Gym Appointment',
+        day: 'July 9th at 12:21',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Soccer Match Appointment',
+        day: 'July 9th at 12:21',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: 'Free time Appointment',
+        day: 'July 9th at 12:21',
+        reminder: true
+    }
+]
